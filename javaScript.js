5f2afdece4b116f69635fe85ad0eef0ba0920a66
developerMasum <developer.masum2001@gmail.com>
2023-03-04 12:13:39 +0600

    close button added

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,31 @@
 const loadAllAiUniverse = () => {
-  const url = `https://openapi.programming-hero.com/api/ai/tools`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayAll(data.data.tools.slice(0, 6)));
-};
-
-const displayAll = (universes) => {
-  console.log(universes[0].published_in);
-
-  const containerDiv = document.getElementById("card-container");
-  containerDiv.innerHTML = "";
-  universes.forEach((universe) => {
-    // console.log(universe.features[0]);
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("col");
-
-    const { image, name, published_in, id } = universe;
-
-    cardDiv.innerHTML = `
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayAll(data.data.tools.slice(0, 6)));
+  };
+  
+  const displayAll = (universes) => {
+    console.log(universes[0].published_in);
+  
+  
+  
+  
+    
+  
+    const containerDiv = document.getElementById("card-container");
+    containerDiv.innerHTML = "";
+    universes.forEach((universe) => {
+      // console.log(universe.features[0]);
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add("col");
+  
+  
+      
+  
+      const { image, name, published_in, id } = universe;
+  
+      cardDiv.innerHTML = `
           <div class="col">
                     <div class="card h-100">
                       <img src="${image}" class="card-img-top" alt="...">
@@ -48,58 +56,63 @@ const displayAll = (universes) => {
                     </div>
                   </div>
           `;
-
-    containerDiv.appendChild(cardDiv);
-  });
-};
-
-// show all bepar separ and  button for all data
-
-const showAllUniverses = async () => {
-  const url = `https://openapi.programming-hero.com/api/ai/tools`;
-  const res = await fetch(url);
-  const data = await res.json();
-  displayAll(data.data.tools);
-
-  // show all button
+  
+      containerDiv.appendChild(cardDiv);
+    });
+  };
+  
+  // show all bepar separ and  button for all data 
+  
+  const showAllUniverses = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAll(data.data.tools);
+  
+    // show all button 
   const button = document.getElementById("button-all");
   button.classList.add("d-none");
   toggleLoader(true);
-};
-//  toggle loader ---
-
-const toggleLoader = (isLoading) => {
-  const loader = document.getElementById("loader");
-  if (isLoading) {
-    loader.classList.remove("d-none");
+   
+  };
+  //  toggle loader ---
+  
+  const toggleLoader = isLoading =>{
+    const loader = document.getElementById('loader');
+    if (isLoading) {
+      loader.classList.remove('d-none')
+      
+    }
+  
   }
-};
-
-loadAllAiUniverse();
-//  hide this spinner ----
-
-// modal started from here **-------------
-
-const fetchDataDetail = (ai_id) => {
-  const url = `https://openapi.programming-hero.com/api/ai/tool/${ai_id}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayDataDetails(data.data));
-  //    console.log(url);
-};
-const displayDataDetails = (details) => {
-  console.log(details);
-  // const title = document.getElementById('description');
-  // title.innerText = details.description;
-
-  const fullContainer = document.getElementById("container-all");
-  const score = details.accuracy.score;
-  const number = score * 100;
-
-  // console.log(number);
-  // const div = document.createElement('div');
-  // div.classList.add('d-flex text-center')
-  fullContainer.innerHTML = `
+  
+  loadAllAiUniverse();
+  //  hide this spinner ----
+  
+  
+  // modal started from here **-------------
+  
+  const fetchDataDetail = (ai_id) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${ai_id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayDataDetails(data.data));
+    //    console.log(url);
+  };
+  const displayDataDetails = (details) => {
+    console.log(details);
+    // const title = document.getElementById('description');
+    // title.innerText = details.description;
+  
+    const fullContainer = document.getElementById("container-all");
+    const score = details.accuracy.score;
+    const number =score * 100;
+    
+  
+    // console.log(number);
+    // const div = document.createElement('div');
+    // div.classList.add('d-flex text-center')
+    fullContainer.innerHTML = `
     
     
     <div class="d-flex gap-2 justify-content-between  ">
@@ -110,23 +123,13 @@ const displayDataDetails = (details) => {
   
          <div class="d-flex">
          <div class=" bg-light me-3 text-danger ">
-           <p>${
-             details.pricing[0].price === "no cost" || "0"
-               ? "Free Of Costs"
-               : details.pricing[0].price
-           }</p>
+           <p>${details.pricing[0].price==='no cost'|| '0' ? "Free Of Costs": details.pricing[0].price}</p>
           </div> 
          <div class=" bg-light me-3 text-success ">
-         <p>${
-           details.pricing[1].price === "no cost" || "0"
-             ? "Free Of Costs"
-             : details.pricing[0].price
-         }</p>
+         <p>${details.pricing[1].price==='no cost' || '0' ? "Free Of Costs": details.pricing[0].price}</p>
           </div> 
          <div class=" bg-light me-3 text-warning  ">
-         <p>${
-           details.pricing[2].price ? details.pricing[2].price : "Free Of Costs"
-         }</p>
+         <p>${details.pricing[2].price ? details.pricing[2].price : "Free Of Costs"}</p>
           </div> 
   
        </div>
@@ -136,25 +139,17 @@ const displayDataDetails = (details) => {
        <div>
            <h3>Feature </h3>
            <ul>
-               <li>${details.features["1"].feature_name}</li>
-               <li>${details.features["2"].feature_name}</li>
-               <li>${details.features["3"].feature_name}</li>
+               <li>${details.features['1'].feature_name}</li>
+               <li>${details.features['2'].feature_name}</li>
+               <li>${details.features['3'].feature_name}</li>
            </ul>
        </div>
        <div>
            <h3>Integration</h3>
            <ul>
-               <li>${details.integrations[0]}</li>
-               <li>${
-                 details.integrations[1]
-                   ? details.integrations[1]
-                   : "No data found"
-               }</li>
-               <li>${
-                 details.integrations[2]
-                   ? details.integrations[2]
-                   : "No data found"
-               }</li>
+               <li>${ details.integrations[0]}</li>
+               <li>${details.integrations[1] ? details.integrations[1]:'No data found'}</li>
+               <li>${details.integrations[2] ? details.integrations[2]:'No data found'}</li>
            </ul>
        </div>
    
@@ -172,9 +167,7 @@ const displayDataDetails = (details) => {
         }"  class="img-fluid "  alt="...">  
         
         <div id="accuracy" class="text-end ">
-       <span class="badge text-bg-warning">${
-         number ? number + "% accuracy" : ""
-       }</span>
+       <span class="badge text-bg-warning">${number? number+'% accuracy': ''}</span>
   
         </div>
         <div class="card-body">
@@ -189,8 +182,17 @@ const displayDataDetails = (details) => {
     
     
     `;
-};
-
-fetchDataDetail();
-
-//  sort ar kaj baj ===
+  };
+  
+  
+  
+  
+  fetchDataDetail();
+  
+  
+  
+  //  sort ar kaj baj === 
+  
+  
+  
+  
